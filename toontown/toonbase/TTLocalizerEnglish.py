28d078d8844fab750dde318b74e3c64204778c7c
Nathan <therage500@gmail.com>
2015-08-19 00:03:27 -0500

    Get rid of special TTI SOS cards

--- toontown/toonbase/TTLocalizerEnglish.py
+++ toontown/toonbase/TTLocalizerEnglish.py
@@ -7617,10 +7617,7 @@ NPCToonNames = {20000: 'Tutorial Tom',
  7006: 'Gimmy Ahand',
  7007: 'Dewin Tymme',
  7008: 'Ima Cagedtoon',
- 7009: 'Jimmy Thelock',
-# Start with 91913.
-91917: 'Magic Cat',
-91918: 'Trap Cat'}
+ 7009: 'Jimmy Thelock'}
 zone2TitleDict = {2513: ('Toon Hall', ''),
  2514: ('Toontown Bank', ''),
  2516: ('Toontown School House', ''),
