23ca42c9a0edd700957c5e3b1c692d61ddc76778
nate97 <nathanielfuhr@gmail.com>
2018-02-22 17:50:13 -0600

    Fix hash value...

--- otp/distributed/DCClassImports.py
+++ otp/distributed/DCClassImports.py
@@ -2,7 +2,7 @@
 from panda3d.core import *
 
 
-hashVal = 2878478080
+hashVal = 624388052
 
 
 from toontown.cogdominium import DistributedCogdoInterior, DistributedCogdoBattleBldg, DistributedCogdoElevatorExt, DistributedCogdoElevatorInt, DistributedCogdoBarrel, DistCogdoGame, DistCogdoLevelGame, DistCogdoBoardroomGame, DistCogdoCraneGame, DistCogdoMazeGame, DistCogdoFlyingGame, DistCogdoCrane, DistCogdoCraneMoneyBag, DistCogdoCraneCog
