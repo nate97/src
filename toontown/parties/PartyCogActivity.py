98b08f95648e6ac5021cc481649c4aca72156f0b
Nathaniel Fuhr <nathanielfuhr@gmail.com>
2019-03-27 18:21:35 -0500

    Thanks for more party AIs

--- toontown/parties/PartyCogActivity.py
+++ toontown/parties/PartyCogActivity.py
@@ -263,6 +263,11 @@ class PartyCogActivity(DirectObject):
             pos = self._doorStartPos[team]
         else:
             pos = (self._doorStartPos[team] + Point3(0, 0, -7.0),)
+            pos = Point3(0, 0, -7.0)
+            
+        print "!!!"
+        print pos
+
         ival = self._arenaDoors[team].posInterval(0.75, pos, blendType='easeIn')
         self._arenaDoorIvals[team] = ival
         ival.start()
@@ -399,7 +404,7 @@ class PartyCogActivity(DirectObject):
     def playPlayerEnterIval(self):
 
         def conditionallyShowSwitchButton(self = self, enable = True):
-            if enable and self.activity.activityFSM.state in ['WaitForEnough', 'WaitToStart']:
+            if enable and self.activity.activityFSM.state_ in ['WaitForEnough', 'WaitToStart']:
                 self.activity.teamActivityGui.enableSwitchButton()
             else:
                 self.activity.teamActivityGui.disableSwitchButton()
